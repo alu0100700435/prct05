e85f450fdd159eb97de9c53adcc2fe2af3332ce7
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-10-18 18:38:28 +0100

    metodo test_simple

--- tc_racional.rb
+++ tc_racional.rb
@@ -8,4 +8,21 @@ class TestFraction < Test::Unit::TestCase
     @f2 = Fraction.new(4,9)
   end
   
+  def tear_down
+     # nothing
+  end
+  
+  def test_simple
+     
+    assert_equal("2/7", @f1.to_s)
+    assert_equal("4/9", (@f2).to_s)
+    assert_equal("46/63", @f1.suma(@f2).to_s)
+    assert_equal("-10/63", @f1.resta(@f2).to_s)
+    assert_equal("8/63", @f1.multiplicacion(@f2).to_s)
+    assert_equal("18/28", @f1.division(@f2).to_s)
+    
+   end
+   
+   
+  
 end
