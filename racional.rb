8a0368b6f88d09e23f4a5765987af51bb8ba6ed1
Raquel Alvarez <alu0100700435@ull.edu.es>
2013-10-18 18:22:32 +0100

    metodo initialize

--- racional.rb
+++ racional.rb
@@ -3,4 +3,18 @@
 require "./gcd.rb"
 
 class Fraccion
+   attr_reader :x, :y
+
+  # Metodo que llama al contructor, que generara una excepcion si el denominador es 0
+  def initialize(x, y)
+    raise RuntimeError unless x.is_a? Numeric and y.is_a? Numeric
+    @x = x
+    if y == 0
+      raise 'No se puede dividir por cero'
+      @y = 1
+    else
+      @y = y
+    end                
+  end
+  
 end
