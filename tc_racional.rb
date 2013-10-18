21857b637e4fa6dba8631b5ca199f644cc726526
alu0100700435 <alu0100700435@ull.edu.es>
2013-10-18 19:40:21 +0100

    mejoras

--- tc_racional.rb
+++ tc_racional.rb
@@ -14,7 +14,7 @@ class TestFraction < Test::Unit::TestCase
   
   def test_simple 
     assert_equal("2/7", @f1.to_s)
-    assert_equal("4/9", (@f2).to_s)
+    assert_equal("4/9", @f2.to_s)
     assert_equal("46/63", @f1.suma(@f2).to_s)
     assert_equal("-10/63", @f1.resta(@f2).to_s)
     assert_equal("8/63", @f1.multiplicacion(@f2).to_s)
